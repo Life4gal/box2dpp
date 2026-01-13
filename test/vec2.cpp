358089b053296176f33416ba2e19441ea6094601
Life4gal <life4gal@gmail.com>
2026-01-13 23:08:20 +0800

    make compiler happy :(

--- test/vec2.cpp
+++ test/vec2.cpp
@@ -17,70 +17,70 @@ namespace
 		constexpr Vec2 two{.x = 2.f, .y = 2.f};
 		constexpr Vec2 m_one{.x = -1.f, .y = -1.f};
 
-		"operator-(unary)"_test = [] noexcept -> void
+		"operator-(unary)"_test = [&] noexcept -> void
 		{
 			constexpr auto o = -m_one;
 			expect(o.x == value(-m_one.x));
 			expect(o.y == value(-m_one.y));
 		};
 
-		"operator-(vec2)"_test = [] noexcept -> void
+		"operator-(vec2)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = zero - two;
 			expect(v.x == value(zero.x - two.x));
 			expect(v.y == value(zero.y - two.y));
 		};
 
-		"operator-(scalar)"_test = [] noexcept -> void
+		"operator-(scalar)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = one - 2;
 			expect(v.x == value(one.x - 2));
 			expect(v.y == value(one.y - 2));
 		};
 
-		"operator+(unary)"_test = [] noexcept -> void
+		"operator+(unary)"_test = [&] noexcept -> void
 		{
 			const auto o = +m_one;
 			expect(o.x == value(std::abs(m_one.x)));
 			expect(o.y == value(std::abs(m_one.y)));
 		};
 
-		"operator+(vec2)"_test = [] noexcept -> void
+		"operator+(vec2)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = one + two;
 			expect(v.x == value(one.x + two.x));
 			expect(v.y == value(one.y + two.y));
 		};
 
-		"operator+(scalar)"_test = [] noexcept -> void
+		"operator+(scalar)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = one + 2;
 			expect(v.x == value(one.x + 2));
 			expect(v.y == value(one.y + 2));
 		};
 
-		"operator*(vec2)"_test = [] noexcept -> void
+		"operator*(vec2)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = one * two;
 			expect(v.x == value(one.x * two.x));
 			expect(v.y == value(one.y * two.y));
 		};
 
-		"operator*(scalar)"_test = [] noexcept -> void
+		"operator*(scalar)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = one * 2;
 			expect(v.x == value(one.x * 2));
 			expect(v.y == value(one.y * 2));
 		};
 
-		"operator/(vec2)"_test = [] noexcept -> void
+		"operator/(vec2)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = one / two;
 			expect(v.x == value(one.x / two.x));
 			expect(v.y == value(one.y / two.y));
 		};
 
-		"operator/(scalar)"_test = [] noexcept -> void
+		"operator/(scalar)"_test = [&] noexcept -> void
 		{
 			constexpr auto v = one / 2;
 			expect(v.x == value(one.x / 2));
