f37210e040f83ce840c8aa47546f3fe6d6a4ab47
Life4gal <life4gal@gmail.com>
2026-01-15 22:08:20 +0800

    math = math.vec2 + math.rotation + math.transform

---
+++ include/box2dpp/math.hpp
@@ -0,0 +1,10 @@
+// This file is part of box2dpp
+// Copyright (C) 2025 Life4gal <life4gal@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#pragma once
+
+#include <box2dpp/math/vec2.hpp>
+#include <box2dpp/math/rotation.hpp>
+#include <box2dpp/math/transform.hpp>
