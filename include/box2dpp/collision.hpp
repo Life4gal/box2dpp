7877fb16a0436ce0405c28356e6bb5cc7dcd17de
Life4gal <life4gal@gmail.com>
2026-01-15 22:09:36 +0800

    collision = circle + capsule + hull + polygon + segment + aabb + cast + mass_data + simplex + distance

---
+++ include/box2dpp/collision.hpp
@@ -0,0 +1,18 @@
+// This file is part of box2dpp
+// Copyright (C) 2025 Life4gal <life4gal@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+#pragma once
+
+#include <box2dpp/collision/circle.hpp>
+#include <box2dpp/collision/capsule.hpp>
+#include <box2dpp/collision/hull.hpp>
+#include <box2dpp/collision/polygon.hpp>
+#include <box2dpp/collision/segment.hpp>
+#include <box2dpp/collision/aabb.hpp>
+
+#include <box2dpp/collision/cast.hpp>
+#include <box2dpp/collision/mass_data.hpp>
+#include <box2dpp/collision/simplex.hpp>
+#include <box2dpp/collision/distance.hpp>
